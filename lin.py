165f75467ecdc7a2f420dc65ec5154db62e09809
LiMo Kiprono <94519855+limo39@users.noreply.github.com>
2023-03-14 23:14:14 +0300

    Update lin.py

--- lin.py
+++ lin.py
@@ -6,14 +6,14 @@ import time
 driver = webdriver.Chrome()
 
 # navigate to the page with the link
-driver.get("https://aif.la/3Ir4k84")
+driver.get("https://your_link")
 
 # find the link element by its CSS selector
 link = driver.find_element(By.CSS_SELECTOR, "a.link-class")
 
 for i in range(102):
     link.click()
-    # time.sleep(1)
+    time.sleep(1)
 
 driver.quit()
 
